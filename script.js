edefb25fffc6f7d9c9a1f88002c61da3b58f5186
GabrielaBatistadaRocha <168228731+GabrielaBatistadaRocha@users.noreply.github.com>
2024-11-08 17:48:05 -0300

    bfghgrg

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ let numero1= parseInt(Math.random()*50+1);
 let numero2= parseInt(Math.random()*50+1);
 
 document.getElementById("nsoma").innerHTML=numero1 + " + " + numero2;
+
+function teste (){
+    let result = document.getElementById("result").value;
+
+    if (numero1 + numero2 == result) {
+        alert ("Login realizado com sucesso!");
+    } else{
+        alert ("A soma está incorreta, tente novamente");
+    }
+}
